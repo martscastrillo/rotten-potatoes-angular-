7ab653a6c911394a18920dcd22fbdea61ad6ade6
Marta Castrillo <mart.castrillo@gmail.com>
2023-08-18 19:23:10 +0200

    add login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  username = "";
+  password = "";
+  errorMsg = " ";
+
+  login(){
+    if ((this.password.trim().length === 0) && (this.username.trim().length === 0)) {
+      this.errorMsg = "Username and password are required";
+    }
+  else if (this.password.trim().length === 0) {
+      this.errorMsg = "Password is required";
+    } 
+    else if (this.username.trim().length === 0) {
+      this.errorMsg = "Username is required";
+    }else {
+      this.errorMsg = "";
+      /* let res = this.auth.login(this.username, this.password);
+      if (res === 200) {
+        this.router.navigate(['home']);
+      }
+      if (res === 403) {
+        this.errorMsg = "Invalid Credentials";
+      } */
+    }
+  }
+}
